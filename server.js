add2a6f4179bdfbb3d90451534f4405144650202
Peter Smith <infitude>
2015-03-10 14:19:39 +1300

    added mongoose

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http')
+var mongoose = require('mongoose');
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });

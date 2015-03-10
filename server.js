fe5982855ee896a1a00bf59b56279af0026105d4
Peter Smith <infitude>
2015-03-10 15:12:10 +1300

    connect

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http')
 var mongoose = require('mongoose');
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
+
+  mongoose.connect('mongodb://user1:user1@ds041177.mongolab.com:41177/lpdb');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World and Pete\n');
 }).listen(port);
